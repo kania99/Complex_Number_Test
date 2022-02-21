ee9e2461a76bd4dd74939bd4a60d9726a37da829
kania99 <maciejkaniewski1999@gmail.com>
2022-02-21 23:24:44 +0100

    Implement whole ComplexNumber

--- inc/complex_number.h
+++ inc/complex_number.h
@@ -2,6 +2,7 @@
 #define COMPLEX_NUMBER_H_
 
 #include <iostream>
+#include <cmath>
 
 /**
  * @brief Model the concept of a complex number
@@ -15,11 +16,64 @@ struct ComplexNumber
 
 /**
  * @brief Add two complex numbers
- * 
+ *
  * @param number1 first complex number
  * @param number2 second complex number
  * @return ComplexNumber result
  */
 ComplexNumber operator+(const ComplexNumber &number1, const ComplexNumber &number2);
+
+/**
+ * @brief Subtract two complex numbers
+ *
+ * @param number1 first complex number
+ * @param number2 second complex number
+ * @return ComplexNumber result
+ */
+ComplexNumber operator-(const ComplexNumber &number1, const ComplexNumber &number2);
+
+/**
+ * @brief Multiply two complex numbers
+ *
+ * @param number1 first complex number
+ * @param number2 second complex number
+ * @return ComplexNumber result
+ */
+ComplexNumber operator*(const ComplexNumber &number1, const ComplexNumber &number2);
+
+/**
+ * @brief Divide two complex numbers
+ *
+ * @param number1 first complex number
+ * @param number2 second complex number
+ * @return ComplexNumber result
+ */
+ComplexNumber operator/(const ComplexNumber &number1, const ComplexNumber &number2);
+
+/**
+ * @brief Calculate modulus of complex number
+ *
+ * @param number complex number
+ * @return double result
+ */
+double Modulus(const ComplexNumber &number);
+
+/**
+ * @brief Display complex number
+ * 
+ * @param output stream
+ * @param number complex number
+ * @return std::ostream& 
+ */
+std::ostream &operator<<(std::ostream &output, const ComplexNumber &number);
+
+/**
+ * @brief Allow to input complex number
+ * 
+ * @param input stream
+ * @param number complex number
+ * @return std::ostream& 
+ */
+std::istream &operator>>(std::istream &input, ComplexNumber &number);
 
 #endif // COMPLEX_NUMBER_H_
--- src/complex_number.cc
+++ src/complex_number.cc
@@ -9,3 +9,50 @@ ComplexNumber operator+(const ComplexNumber &number1, const ComplexNumber &numbe
 
     return result;
 }
+
+ComplexNumber operator-(const ComplexNumber &number1, const ComplexNumber &number2)
+{
+    ComplexNumber result;
+
+    result.real = number1.real - number2.real;
+    result.imag = number1.imag - number2.imag;
+
+    return result;
+}
+
+ComplexNumber operator*(const ComplexNumber &number1, const ComplexNumber &number2)
+{
+    ComplexNumber result;
+
+    result.real = number1.real * number2.real - number1.imag * number2.imag;
+    result.imag = number1.imag * number2.imag + number1.imag * number2.real;
+
+    return result;
+}
+
+ComplexNumber operator/(const ComplexNumber &number1, const ComplexNumber &number2)
+{
+    ComplexNumber result;
+
+    result.real = (number1.real * number2.real + number1.imag * number2.imag) / pow(Modulus(number2), 2);
+    result.imag = (number1.imag * number2.real - number1.real * number2.imag) / pow(Modulus(number2), 2);
+
+    return result;
+}
+
+double Modulus(const ComplexNumber &number)
+{
+    return sqrt(pow(number.real, 2) + pow(number.imag, 2));
+}
+
+std::ostream &operator<<(std::ostream &output, const ComplexNumber &number)
+{
+    output << "(" << number.real << std::showpos << number.imag << "i)" << std::noshowpos;
+    return output;
+}
+
+std::istream &operator>>(std::istream &input, ComplexNumber &number)
+{
+    input >> number.real >> number.imag;
+    return input;
+}
